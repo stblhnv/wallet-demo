4c4eb5469aba56d73487b9df7217a5d568c76753
Stanislav Balahonov <stanislav.balahonov@simbirsoft.com>
2020-01-21 18:38:48 +0400

    feature/models: postgresql adjusted

--- wallet_demo/settings.py
+++ wallet_demo/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'wallet_demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wallet_demo_db',
+        'USER': 'postgresql_user',
+        'PASSWORD': 'postgresql_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
+    },
 }
 
 
