5ca97611e4446bdcf78581b526e977f765b0bfdf
Stanislav Balahonov <stanislav.balahonov@simbirsoft.com>
2020-01-22 15:50:40 +0400

    feature/endpoint_user_and_wallet_creation: tests added

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -1,8 +1,10 @@
 import pytest
+from decimal import Decimal
 
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.test import APIRequestFactory
 
+from api.views import UserRegistrationView
 from customauth.models import CustomUser
 
 
@@ -53,3 +55,90 @@ def test_signin__proper_call__return_token():
     # assert
     assert response.status_code == 200
     assert 'token' in response.data.keys()
+
+
+@pytest.mark.django_db
+def test_signup__proper_call__return_200_and_message(mocker):
+    # arrange
+    client = APIRequestFactory()
+    data = {
+        'email': 'test@test.com',
+        'password1': 'test',
+        'password2': 'test',
+        'currency': 'RUB',
+        'init_balance': Decimal('50.00')
+    }
+    mocker.patch('api.views.create_user_and_wallet')
+    view = UserRegistrationView.as_view()
+    request = client.post(
+        '/api/v1/signup/',
+        data=data,
+        format='json',
+    )
+
+    # act
+    response = view(request)
+
+    # assert
+    assert response.status_code == 201
+    assert response.data['status'] == 201
+    assert response.data['message'] == 'User is successfully created'
+
+
+@pytest.mark.django_db
+def test_signup__value_error__return_400_and_exception_text(mocker):
+    # arrange
+    client = APIRequestFactory()
+    data = {
+        'email': 'test@test.com',
+        'password1': 'test',
+        'password2': 'test',
+        'currency': 'RUB',
+        'init_balance': Decimal('50.00')
+    }
+    mocker.patch(
+        'api.views.create_user_and_wallet',
+        side_effect=Exception('Something went wrong'),
+    )
+    view = UserRegistrationView.as_view()
+    request = client.post(
+        '/api/v1/signup/',
+        data=data,
+        format='json',
+    )
+
+    # act
+    response = view(request)
+
+    # assert
+    assert response.status_code == 400
+    assert response.data['error'] == 'Something went wrong'
+
+
+@pytest.mark.django_db
+def test_signup__email_unfilled__return_400_and_exception_raised(mocker):
+    # arrange
+    client = APIRequestFactory()
+    data = {
+        'password1': 'test',
+        'password2': 'test',
+        'currency': 'RUB',
+        'init_balance': Decimal('50.00')
+    }
+    create_user_and_wallet_mocker = mocker.patch(
+        'api.views.create_user_and_wallet',
+    )
+    view = UserRegistrationView.as_view()
+    request = client.post(
+        '/api/v1/signup/',
+        data=data,
+        format='json',
+    )
+
+    # act
+    response = view(request)
+
+    # assert
+    assert response.status_code == 400
+    assert response.data['status'] == 400
+    create_user_and_wallet_mocker.assert_not_called()
