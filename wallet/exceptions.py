84f0f390ec64eb9cdf2fd12e02a805b4adb5fe51
Stanislav Balahonov <stanislav.balahonov@simbirsoft.com>
2020-01-22 14:34:36 +0400

    feature/endpoint_user_and_wallet_creation: create_wallet added

--- wallet/exceptions.py
+++ wallet/exceptions.py
@@ -1,2 +1,6 @@
 class ExchangeRateCreationException(Exception):
     pass
+
+
+class WalletCreationException(Exception):
+    pass
--- wallet/services.py
+++ wallet/services.py
@@ -1,9 +1,18 @@
+from decimal import (
+    Decimal,
+    ROUND_HALF_DOWN,
+)
 from typing import Dict
 
-from .exceptions import ExchangeRateCreationException
+from customauth.models import CustomUser
+from .exceptions import (
+    ExchangeRateCreationException,
+    WalletCreationException,
+)
 from .models import (
     CURRENCIES,
     ExchangeRate,
+    Wallet,
 )
 from .repositories.exchangeratesapi import get_exchange_rates
 
@@ -33,3 +42,20 @@ def update_exchange_rates() -> None:
             currency=currency,
             exchange_rates=new_rates,
         )
+
+
+def create_wallet(
+        user: CustomUser,
+        currency: str,
+        init_balance: Decimal,
+) -> Wallet:
+    if init_balance >= 0 and currency in CURRENCIES:
+        cents = Decimal('.01')
+        return Wallet.objects.create(
+            owner=user,
+            currency=currency,
+            balance=Decimal(init_balance).quantize(cents, ROUND_HALF_DOWN),
+        )
+    raise WalletCreationException(
+        'Unable to create wallet with negative balance or currency is unknown',
+    )
