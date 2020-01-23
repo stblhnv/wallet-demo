615ab34958180088997b2f3bc9fda5f91675ea35
Stanislav Balahonov <stanislav.balahonov@simbirsoft.com>
2020-01-23 13:17:42 +0400

    feature/transmit_money: transfer mpney url added

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from rest_framework.authtoken.views import ObtainAuthToken
 
-from .views import UserRegistrationView
+from .views import (
+    TransmitMoneyView,
+    UserRegistrationView,
+)
 
 
 urlpatterns = [
     path('v1/signin/', ObtainAuthToken.as_view()),
     path('v1/signup/', UserRegistrationView.as_view()),
+    path('v1/transmit_money/', TransmitMoneyView.as_view()),
 ]
