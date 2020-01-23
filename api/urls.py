d59c88999fe714d29f4c80afb0d7e5ab4cae30e5
Stanislav Balahonov <stanislav.balahonov@simbirsoft.com>
2020-01-23 15:37:19 +0400

    feature/wallet_operations: added WalletTransactionsView

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from .views import (
     TransmitMoneyView,
     UserRegistrationView,
+    UserTransactionsView,
 )
 
 
@@ -11,4 +12,5 @@ urlpatterns = [
     path('v1/signin/', ObtainAuthToken.as_view()),
     path('v1/signup/', UserRegistrationView.as_view()),
     path('v1/transmit_money/', TransmitMoneyView.as_view()),
+    path('v1/transactions/<int:wallet_id>/', UserTransactionsView.as_view()),
 ]
