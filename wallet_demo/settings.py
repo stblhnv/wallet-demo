9ff07ceb38a8fd768fec8e9fa2fe7693d63c9e7e
Stanislav Balahonov <stanislav.balahonov@simbirsoft.com>
2020-02-02 20:30:24 +0400

    feature/cron: added apscheduler

--- wallet_demo/settings.py
+++ wallet_demo/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_apscheduler',
     'rest_framework',
     'rest_framework.authtoken',
     'api',
     'customauth',
+    'scheduler',
     'wallet',
 ]
 
